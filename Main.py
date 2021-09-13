e35ea3e7ae7fafd30273d15404dd88a027c2931b
Isak Juhl Nielsen <isak.juhl.nielsen@gmail.com>
2021-09-13 11:02:56 +0200

    beginning of GUI

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import Tkinter as tk
 
 con = sqlite3.connect('Data.db')
 print("Data being stored")
@@ -10,3 +11,22 @@ try:
 except Exception:
     print("Table already exists")
 
+class graphicDobot(Tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+
+        ButtHeight = 50
+        Buttwidth = 50
+
+        pos1col = "Black"
+        pos1 = tk.Button(self, bg = pos1col, command = , Height = ButtHeight, Width = Buttwidth)
+        
+        pos2col = "Black"
+        pos2 = tk.Button(self, bg = pos2col, command = , Height = ButtHeight, Width = Buttwidth)
+
+        pos3col = "Black"
+        pos3 = tk.Button(self, bg = pos3col, command = , Height = ButtHeight, Width = Buttwidth)
+
+        pos4col = "Black"
+        pos4 = tk.Button(self, bg = pos1col, command = , Height = ButtHeight, Width = Buttwidth)
+        
